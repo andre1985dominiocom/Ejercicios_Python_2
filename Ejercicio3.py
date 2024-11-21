6027960b9dd3212841ef88dcd22d1d074de44975
andre1985dominiocom <sergio19855@gmail.com>
2024-11-21 14:31:00 -0500

    Ejercicios_Phyton_2

---
+++ Ejercicio3.py
@@ -0,0 +1,14 @@
+#Imprimir título
+print("Programa para registrar rango de calificaciones:")
+Excelente=float(input("Ingrese la calificación:"))
+Muy_Bien=float(input("Ingrese la calificación:"))
+Bien=float(input("Ingrese la calificación:"))
+No_Aprobado=float(input("Ingrese la calificación:"))
+if Excelente>9.1 and 10:
+  print("Mostrar A")
+elif Muy_Bien>8.1 and 9:
+  print("Mostrar A")
+elif Bien>7.5 and 8:
+  print("Mostrar A")
+else:
+  print("Mostrar No Aprobado")
---
+++ Ejercicio4.py
@@ -0,0 +1,7 @@
+Nombre_Mes=(input("Ingresar el nombre del mes:"))
+Signo_1=("Capricornio")
+Signo_2=("Aries")
+if Nombre_Mes==Signo_1:
+  print("Mostrar el nombre del signo zodiacal:" , Signo_1)
+else:
+  print("Mostrar el nombre del signo zodiacal:" , Signo_2)
