99f1854b6e316a235ff9bf199727e6c39edd3faa
andre1985dominiocom <sergio19855@gmail.com>
2024-11-21 16:00:29 -0500

    Ejercicios_Python_2

---
+++ Ejercicio7.py
@@ -0,0 +1,11 @@
+Menor=19
+Igual=19
+Estatura=175
+Peso=75 and 80
+if Menor<19 and Igual==19:
+  print("Mostrar las edades:")
+elif Estatura>175 and Peso>75 and 80:
+  print("Mostrar estaura y peso:")
+else:
+  print("Mostrar la variables exigidas en el equipo" , Menor , Igual , Estatura , Peso)
+  
