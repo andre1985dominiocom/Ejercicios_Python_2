6f82a92fb2f5dd9c44437ecea35bfbbef850524f
andre1985dominiocom <sergio19855@gmail.com>
2024-11-21 15:33:44 -0500

    Ejercicios_Python_2

---
+++ Ejercicio6.py
@@ -0,0 +1,7 @@
+import math
+Angulo_Grados=float(input("Mostrar el angulo el grados:"))
+Angulo_Radianes=float(input("Mostrar el angulo en radianes:"))
+if (Angulo_Grados*math.pi)/180:
+  print("Calcular el resultado en grados:")
+elif (Angulo_Radianes*math.pi)/180:
+  print("Calcular el resultado en radianes:")
