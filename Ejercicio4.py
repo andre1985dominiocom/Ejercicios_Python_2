2455dcf9e35e74aece7a849c0dae010415ba2721
andre1985dominiocom <sergio19855@gmail.com>
2024-11-21 14:59:05 -0500

    Ejercicios_Python_2

--- Ejercicio4.py
+++ Ejercicio4.py
@@ -1,7 +1,39 @@
 Nombre_Mes=(input("Ingresar el nombre del mes:"))
-Signo_1=("Capricornio")
-Signo_2=("Aries")
-if Nombre_Mes==Signo_1:
+Signo_1=("Aries")
+Signo_2=("Tauro")
+Signo_3=("Géminis")
+Signo_4=("Cáncer")
+Signo_5=("Leo")
+Signo_6=("Virgo")
+Signo_7=("Libra")
+Signo_8=("Escorpio")
+Signo_9=("Sagitario")
+Signo_10=("Capricornio")
+Signo_11=("Acuario")
+Signo_12=("Piscis")
+if Nombre_Mes=="Enero":
   print("Mostrar el nombre del signo zodiacal:" , Signo_1)
+if Nombre_Mes=="Febrero":
+  print("Mostrar el nombre dek signo zodiacal:" , Signo_2)
+if Nombre_Mes=="Marzo":
+  print("Mostrar el nombre del signo zodiacal:" , Signo_3)
+if Nombre_Mes=="Abril":
+  print("Mostrar el nombre del signo zodiacal:" , Signo_4)
+if Nombre_Mes=="Mayo":
+  print("Mostrar el nombre del signo zodiacal" , Signo_5)
+if Nombre_Mes=="Junio":
+  print("Mostrar el nombre del signo zodiacal:" , Signo_6)
+if Nombre_Mes=="Julio":
+  print("Mostrar el nombre del signo zodiacal:" , Signo_7)
+if Nombre_Mes=="Agosto":
+  print("Mostrar el nombre del signo zodiacal:" , Signo_8)
+if Nombre_Mes=="Septiembre":
+  print("Mostrar el nombre del signo zodiacal:" , Signo_9)
+if Nombre_Mes=="Octubre":
+  print("Mostrar el nombre del signo zodiacal:" , Signo_10)
+if Nombre_Mes=="Noviembre":
+  print("Mostrar el nombre del signo zodiacal:" , Signo_11) 
+if Nombre_Mes=="Diciembre":
+  print("Mostrar nombre del signo zodiacal:" , Signo_12)
 else:
-  print("Mostrar el nombre del signo zodiacal:" , Signo_2)
+  print("Mostrar nombre signo zodiacal:")
