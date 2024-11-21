37970a7986138f6a06c217989f776bdb1a481f27
andre1985dominiocom <sergio19855@gmail.com>
2024-11-21 13:45:47 -0500

    Ejercicios_Python_2

---
+++ Ejercicio1.py
@@ -0,0 +1,15 @@
+#Imprimir título
+print("Programa para leer formulas para compuestos quimicos:")
+Cloruro_Sodio=("NaCl:")
+Agua=("H2O:")
+Acido_clorhidrico=("HCI:")
+Selección=("NaCl, H2O, HCI:")
+Opcion=("Seleccionar")
+if Opcion==Selección:
+  print("Desplegar el compuesto quimico:" , Cloruro_Sodio)
+elif Opcion==Agua:
+  print("Desplegar el compuesto quimico:" , Agua)
+elif Opcion==Acido_clorhidrico:
+  print("Desplegar el compuesto quimico:" , Acido_clorhidrico)
+else:
+  print("Seleccionar opcion:" , Opcion)
---
+++ Ejercicio2.py
@@ -0,0 +1,9 @@
+#Impimir título
+print("Programa para calcular los intereses que se pagan en banco a los usuarios")
+Saldo_Final=float(input("Mostrar el saldo final con los intereses:"))
+Interes=float(input("Mostrar intereses pagados:"))
+if 100000<0.3:
+  print("Calcular el interes pagado")
+elif 100000>0.4:
+  print("Calcular intereses pagados")
+  
